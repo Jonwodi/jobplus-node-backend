3384c288d00f94c259e2b686942ad858a1c7f8a5
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2023-03-21 18:16:41 +0000

    Created user routes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 const express = require("express");
-
 const app = express();
+const cors = require("cors");
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.json({ type: "application/vnd.api+json" }));
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+const userRoute = require("./routes/user.routes");
+app.use("/api/", userRoute);
 
 module.exports = app;
