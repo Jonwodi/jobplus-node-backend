63ef10319ade267dfc90681049680f887a33e85b
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2023-03-22 16:02:58 +0000

    user validation and password encryption added

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -1,12 +1,30 @@
 const db = require("../config/database");
+const bcrypt = require("bcrypt");
 
 // create user
 exports.createUser = async (body) => {
   const { first_name, last_name, email, password, confirm_password } = body;
 
+  // check if user already exists using user email
+  const checkUserEmail = await db.query(
+    'SELECT * FROM "users" WHERE email = $1',
+    [email]
+  );
+  if (checkUserEmail.rows.length > 0) {
+    throw new Error("User already exists");
+  }
+
+  // check if passwords match
+  if (password !== confirm_password) {
+    throw new Error("Passwords do not match");
+  }
+
+  // hash password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
   const { rows } = await db.query(
     'INSERT INTO "users" (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING *',
-    [first_name, last_name, email, password]
+    [first_name, last_name, email, hashedPassword]
   );
 
   return rows;
